788e91fa612d40bc3368ceeea1e3794a79e5df3f
bala <sbalak97@gmail.com>
2023-05-03 23:17:40 +0530

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse non tristique nunc. Nunc interdum felis ex, et tincidunt justo efficitur sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris a feugiat nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque a fringilla turpis, pellentesque imperdiet elit. Nam vulputate efficitur velit, at sollicitudin felis fringilla eget. Aliquam semper elementum nisl pellentesque laoreet. Vivamus volutpat ac augue ut volutpat. Integer elit turpis, tincidunt non sem nec, congue auctor erat. Suspendisse finibus ex hendrerit, viverra diam ac, mattis orci. In efficitur tortor ut tortor molestie, ac viverra libero eleifend. Suspendisse convallis lorem eu neque fermentum vulputate. Aenean ut dui lorem. Pellentesque sed mauris at arcu dapibus rutrum.
+      </p>
+    </section>
+  )
+}
+
+export default About
